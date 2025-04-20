2ffa09ad144804a3d4a7adabf483bc5d1368f4da
MOTIVITION <bernibeleshun25@gmail.com>
2025-04-20 17:50:27 +0000

    Update Solutions.py5240100304

--- Assignment 3/Solutions.py
+++ Assignment 3/Solutions.py
@@ -1,7 +1,6 @@
-5240100304
+
 Solutions to assignment 3
 """
-
 """
 1.Write a Python program to reverse the string "Programming". Print the reversed string.
 Hint: Use string slicing or a loop.
@@ -9,8 +8,6 @@ Hint: Use string slicing or a loop.
 #ans
 name="programming"
 print(names[::-1]))
-
-
 """
 2.Create a Python program that takes a user’s full name as input and prints the initials in uppercase.
 Example: Input: "john doe", Output: "J.D."
@@ -20,7 +17,6 @@ first_name=input("Enter your firstname:"
 second_name=("Enter your secondname:")
 print(f"{first_name[0].upper(}).
 {second_name[0].upper()})
-
 
 """
 3.Write a Python program to check if a given string is a palindrome. A palindrome reads the same forwards
@@ -33,7 +29,6 @@ print("the number is palindrome")
 else:
 print("the number is not palindrome")
 
-
 """
 4.Create a Python program that asks the user to enter a sentence and counts the number of words in the sentence.
 Hint: Use the split() method to break the string into words.
@@ -44,7 +39,6 @@ w=sentence.split()
 words=len(w)
 print("words in your sentence is",words)
 
-
 """
 5.Write a Python program to replace all occurrences of "is" with "was" in the string "This is a string and it
 is an example." Print the modified string.
@@ -52,4 +46,4 @@ is an example." Print the modified string.
 #ans
 sentence="this is string and it is an example "
 chan=sentance.replace(","was")
-print(Chan)
+print(Chan")
